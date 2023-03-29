00da4be3f35cbf5de7866d25b585d65ee8cb30fe
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-29 19:47:41 +0200

    Add App component logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Greeting from './components/Greeting';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route exact path="/greet" element={<Greeting />} />
+    </Routes>
+  )
 }
 
 export default App;
