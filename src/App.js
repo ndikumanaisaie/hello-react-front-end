60cc23d2fca9594b3df87bc348026152a7a25625
ndikumana isaie <ndikumanaisaie@gmail.com>
2023-03-29 20:00:16 +0200

    Import dependecies and add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { NavLink } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import Greeting from './components/Greeting';
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/greet" element={<Greeting />} />
-    </Routes>
+    <BrowserRouter>
+      <NavLink to="/" role="button">
+        <span>Home | </span>
+      </NavLink>
+      <NavLink to="/greet" role="button">
+        <span>Greet someone</span>
+      </NavLink>
+      <Routes>
+        <Route exact path="/greet" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import store from './redux/configureStore';
 import App from './App';
